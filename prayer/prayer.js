efc99e65bc7e3ded7d604d6e2717d7ebc37bd8ae
Nicholas Hurley <hurley@todesschaf.org>
2019-03-09 19:22:07 -0800

    Start work on prayer pages
    This is the text and basic outline of noonday and compline (the easy
    ones). Still needs good formatting,
    a little interactivity, and MP and EP.

---
+++ prayer/prayer.js
@@ -0,0 +1,4 @@
+// TODO: this is totally empty right now. Needs to hide the approprate psalms
+// and collects, or perhaps show the proper psalms and collects. Who knows?
+// Also needs to set things up to be able to show the others, if necessary.
+// Maybe.
