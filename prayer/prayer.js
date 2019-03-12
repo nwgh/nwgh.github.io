45031821b7727724fc5dff3e509bcc5597013c99
Nicholas Hurley <hurley@todesschaf.org>
2019-03-11 18:47:42 -0700

    Set things up to show only the proper stuff for the day

--- prayer/prayer.js
+++ prayer/prayer.js
@@ -2,3 +2,13 @@
 // and collects, or perhaps show the proper psalms and collects. Who knows?
 // Also needs to set things up to be able to show the others, if necessary.
 // Maybe.
+
+function setupPrayer() {
+    let days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+    let today = new Date();
+    let day = days[today.getDay()];
+    let dayParts = document.getElementsByClassName(day);
+    for (let p of dayParts) {
+        p.style = 'display: block;';
+    }
+}
